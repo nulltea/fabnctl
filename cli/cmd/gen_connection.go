c0d2223e6d63f17559e4b8fbd9390f54f03ba6f2
Timothy <timauthx@gmail.com>
2021-06-24 04:54:01 +0300

    Implement gen connection command

---
+++ cli/cmd/gen_connection.go
@@ -0,0 +1,208 @@
+package cmd
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path"
+	"text/template"
+
+	"github.com/pkg/errors"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"github.com/timoth-y/chainmetric-network/cli/model"
+	"sigs.k8s.io/yaml"
+)
+
+// connectionCmd represents the connection command
+var connectionCmd = &cobra.Command{
+	Use:   "connection",
+	Short: "Generates connection configuration file",
+	Long: `Generates connection configuration file
+
+Examples:
+  # Generate:
+  fabnctl gen connection -f ./network-config.yaml`,
+
+	RunE: handleErrors(genConnection),
+}
+
+func init() {
+	genCmd.AddCommand(connectionCmd)
+
+	connectionCmd.Flags().StringP("org", "o", "", "Owner organization name (required)")
+	connectionCmd.Flags().StringP("channel", "c", "", "Channel name (required)")
+	connectionCmd.Flags().StringP("name", "n", "",
+		"Connection profile name (default is {org}-connection)",
+	)
+	connectionCmd.Flags().String("description", "", "Connection profile description")
+	connectionCmd.Flags().Float64P("version", "v", 1.0, "Version for connection profile")
+
+
+	connectionCmd.MarkFlagRequired("org")
+	connectionCmd.MarkFlagRequired("channel")
+}
+
+func genConnection(cmd *cobra.Command, _ []string) error {
+	var (
+		err error
+		configPath string
+		ownerOrg string
+		channel string
+		name string
+		desc string
+		version float64
+		netConfig model.NetworkConfig
+	)
+
+	// Parsing flags:
+	if configPath, err = cmd.Flags().GetString("config"); err != nil {
+		return errors.Wrap(ErrInvalidArgs, "failed to parse 'config' parameter")
+	}
+
+	if ownerOrg, err = cmd.Flags().GetString("org"); err != nil {
+		return errors.Wrap(ErrInvalidArgs, "failed to parse required 'org' parameter")
+	}
+
+	if channel, err = cmd.Flags().GetString("channel"); err != nil {
+		return errors.Wrap(ErrInvalidArgs, "failed to parse required 'channel' parameter")
+	}
+
+	if name, err = cmd.Flags().GetString("name"); err != nil {
+		return errors.Wrap(ErrInvalidArgs, "failed to parse 'name' parameter")
+	}
+
+	if desc, err = cmd.Flags().GetString("description"); err != nil {
+		return errors.Wrap(ErrInvalidArgs, "failed to parse 'description' parameter")
+	}
+
+	if version, err = cmd.Flags().GetFloat64("version"); err != nil {
+		return errors.Wrap(ErrInvalidArgs, "failed to parse 'version' parameter")
+	}
+
+	if len(name) == 0 {
+		name = fmt.Sprintf("%s-connection", ownerOrg)
+	}
+
+	if len(desc) == 0 {
+		desc = fmt.Sprintf("Connection profile configuration for %s owned application", ownerOrg)
+	}
+
+	// Decoding network config file:
+	configYaml, err := ioutil.ReadFile(configPath)
+	if err != nil {
+		return errors.Wrapf(err, "missing configuration values on path %s", configPath)
+	}
+
+	if err = yaml.Unmarshal(configYaml, &netConfig); err != nil {
+		return errors.Wrapf(err, "failed to decode config found on path: %s", configPath)
+	}
+
+	// Enrich network config with TLS CA certificates:
+	var certPath = path.Join(
+		fmt.Sprintf(".crypto-config.%s", netConfig.Domain),
+		"ordererOrganizations", netConfig.Domain,
+		"tlsca", fmt.Sprintf("tlsca.%s-cert.pem", netConfig.Domain),
+	)
+
+	if cert, err := ioutil.ReadFile(certPath); err != nil {
+		cmd.Printf(
+			"%s TLS certificate for '%s' orderer not found on path '%s'\n",
+			viper.GetString("cli.warning_emoji"), netConfig.Orderer.Name, certPath,
+		)
+	} else {
+		netConfig.Orderer.TLSCert = string(cert)
+	}
+
+	// Filter channel and organization based on given channel ID:
+	if ch := netConfig.GetChannel(channel); ch == nil {
+		return errors.Errorf("Channel with ID '%s' isn't defined in given config '%s'", channel, configPath)
+	} else {
+		netConfig.Channels = []model.Channel{*ch}
+	}
+
+	var channelOrgs []model.Organization
+
+	for i, org := range netConfig.Organizations {
+		if netConfig.GetChannel(channel).HasOrganization(org.Name) {
+			channelOrgs = append(channelOrgs, netConfig.Organizations[i])
+		}
+	}
+	netConfig.Organizations = channelOrgs
+
+	if org := netConfig.GetOrganization(ownerOrg); org == nil {
+		return errors.Errorf("Organization with ID '%s' isn't a part of '%s' channel consortium", org, channel)
+	}
+
+	for i, org := range netConfig.Organizations {
+		certPath = path.Join(
+			fmt.Sprintf(".crypto-config.%s", netConfig.Domain),
+			"peerOrganizations", fmt.Sprintf("%s.%s", org.Hostname, netConfig.Domain),
+			"tlsca", fmt.Sprintf("tlsca.%s.%s-cert.pem", org.Hostname, netConfig.Domain),
+		)
+
+		if cert, err := ioutil.ReadFile(certPath); err != nil {
+			cmd.Printf(
+				"%s TLS certificate for '%s' organization not found on path '%s'\n",
+				viper.GetString("cli.warning_emoji"), org.Name, certPath,
+			)
+		} else {
+			netConfig.Organizations[i].TLSCert = string(cert)
+		}
+
+		certPath = path.Join(
+			fmt.Sprintf(".crypto-config.%s", netConfig.Domain),
+			"peerOrganizations", fmt.Sprintf("%s.%s", org.Hostname, netConfig.Domain),
+			"ca", fmt.Sprintf("ca.%s.%s-cert.pem", org.Hostname, netConfig.Domain),
+		)
+
+		if cert, err := ioutil.ReadFile(certPath); err != nil {
+			cmd.Printf(
+				"%s TLS certificate for '%s' organization's CA not found on path '%s'\n",
+				viper.GetString("cli.warning_emoji"), org.Name, certPath,
+			)
+		} else {
+			netConfig.Organizations[i].CertAuthority.TLSCert = string(cert)
+		}
+	}
+
+	// Values for template rendering:
+	type ConnectionValues struct {
+		Name string
+		Description string
+		Version string
+		OwnerOrg string
+		Channel string
+		model.NetworkConfig
+	}
+
+	values := ConnectionValues{
+		Name: name,
+		Description: desc,
+		Version: vtoa(version),
+		OwnerOrg: ownerOrg,
+		Channel: channel,
+		NetworkConfig: netConfig,
+	}
+
+	var (
+		tpl = template.Must(
+			template.New("connection").ParseFiles("cli/template/connection.goyaml"),
+		)
+	)
+
+	// Render template in connection.yaml file
+	file, err := os.Create("connection.yaml")
+	if err != nil {
+		return errors.Wrap(err, "failed to create file 'connection.yaml'")
+	}
+	defer file.Close()
+
+	if err = tpl.ExecuteTemplate(file, "connection.goyaml", values); err != nil {
+		return errors.Wrap(err, "failed to render connection config")
+	}
+
+	cmd.Println("🎉 Connection config generation done!")
+
+	return nil
+}
