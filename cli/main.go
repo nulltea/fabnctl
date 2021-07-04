90f5c47023d29f9d3564cb6189ba8ae17ffc2a39
Timothy <timauthx@gmail.com>
2021-07-04 05:20:05 +0300

    Improvement: update code to suit installation

--- cli/main.go
+++ cli/main.go
@@ -2,12 +2,7 @@ package main
 
 import (
 	"github.com/timoth-y/chainmetric-network/cli/cmd"
-	"github.com/timoth-y/chainmetric-network/cli/shared"
 )
-
-func init() {
-	shared.InitCore()
-}
 
 func main() {
 	cmd.Execute()
